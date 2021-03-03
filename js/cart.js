0feec10750874058ec3983abfbadf171cf328f78
MarwanReyadAL-Khawaldeh <marwanreyad@gmail.com>
2021-03-03 21:09:09 +0200

    cart page

--- js/cart.js
+++ js/cart.js
@@ -24,7 +24,8 @@ function clearCart() {
   clear.innerHTML = '';
 
 }
-let tabelElement = document.createElement('tabel');
+
+let tabelElement = document.getElementsByTagName('tbody')[0];
 
 // TODO: Fill in the <tr>'s under the <tbody> for each item in the cart
 function showCart() {
@@ -36,49 +37,57 @@ function showCart() {
   // TODO: Create a TD for the delete link, quantity,  and the item
   // TODO: Add the TR to the TBODY and each of the TD's to the TR
 
-  for ( let i = 0; i < cart.items.length; i++ ) {
-    let tr = document.createElement( 'tr' );
-    let deleteItem = document.createElement( 'Delete' );
-    let quantity = document.createElement( 'td' );
-    let itemElement = document.createElement( 'td' );
-    let deleteItems = document.createElement( 'td' );
-
-
-
+  for (let i = 0; i < cart.items.length; i++) {
+    let tr = document.createElement('tr');
+    let deleteItem = document.createElement('Delete');
+    let quantity = document.createElement('td');
+    let itemElement = document.createElement('td');
+    let deleteItems = document.createElement('td');
+    let image=document.createElement('td');
     deleteItem.textContent = 'X';
-    deleteItem.setAttribute( 'href', '#' );
-    deleteItem.setAttribute( 'att', cart.items[i].product );
-    quantity.textContent = cart.items[i].quantity;
-    itemElement.textContent = cart.items[i].product;
-    deleteItems.appendChild( deleteItem );
-    tr.appendChild( deleteItems );
-    tr.appendChild( quantity );
-    tr.appendChild( itemElement );
-    table.appendChild( tr );
+    deleteItem.setAttribute('href', '#');
+    deleteItem.setAttribute('att', cart.items[i].itemSelected);
+    quantity.textContent = cart.items[i].quantitySelected;
+    itemElement.textContent = cart.items[i].itemSelected;
+    deleteItems.appendChild(deleteItem);
+    tr.appendChild(deleteItems);
+    tr.appendChild(quantity);
+    tr.appendChild(itemElement);
+    tabelElement.appendChild(tr);
 
   }
 
 
+
+
+
 }
+
+
+
+
 
 function removeItemFromCart(event) {
 
   // TODO: When a delete link is clicked, use cart.removeItem to remove the correct item
   // TODO: Save the cart back to local storage
   // TODO: Re-draw the cart table
-  for ( let i = 0; i < cart.items.length; i++ ) {
-    console.log( event.target.abbr, cart.items[i].product );
-    let att = event.target.getAttribute( 'att' );
-    if ( att === cart.items[i].product ) {
-      cart.removeItem( i );
+  for (let i = 0; i < cart.items.length; i++) {
+    let att = event.target.getAttribute('att');
+    if (att === cart.items[i].itemSelected) {
+      cart.removeItem(i);
 
     }
-    cart.saveToLocalStorage();
-    renderCart();
-  }
 
+    
+    
+
+  }
+cart.saveToLocalStorage();
+renderCart();
 
 }
+
 
 // This will initialize the page and draw the cart on screen
 renderCart();
